3646321f2c8531a37f5678152d1b178ff7def285
Nelson Ramirez <xhyskoax@gmail.com>
2023-12-26 10:37:51 -0300

    Se establece conexión a la base de datos

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require("./app/app.js");
+const db = require("./app/database/config.js");
 require("dotenv").config();
 
 const main = async() => {
     try {
         console.log("iniciando aplicación");
+        await db.sync();
         app.listen(process.env.PORT, () => console.log("Servidor escuchando en puerto: ", process.env.PORT));
     } catch (error) {
         console.log("error al inicializar la aplicación", error)
