6eb7a6027a43eb90949c947e15a008f312273bef
Nelson Ramirez <xhyskoax@gmail.com>
2023-12-26 10:22:56 -0300

    Se corrige código de respuesta en body de 201 a 200 de ruta api usuarios

--- app/routes/usuario.routes.js
+++ app/routes/usuario.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.json({code: 201, message:"Listado de usuarios."});
+  res.json({code: 200, message:"Listado de usuarios."});
 })
 
 module.exports = router;
