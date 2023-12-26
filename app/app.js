59557500b626cbf2af9b233bd2255aa7af8940ba
Nelson Ramirez <xhyskoax@gmail.com>
2023-12-26 10:21:36 -0300

    Se agrega ruta api para usuarios

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const usuariosRoutes = require("./routes/usuario.routes.js")
 
 const app = express();
 
+
+//RUTAS DE LA APP
+app.use("/api/v1/usuarios", usuariosRoutes);
 
 app.all("*", (req, res) => {
     res.status(404).json({code: 404, message: "Ruta no conocida."});
